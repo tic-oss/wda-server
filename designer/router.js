6fbcb5638d80aa455231239d86a3dc1d0d2c14fc
sri-harsha06 <sriharsha.k@comakeit.com>
2023-08-13 00:44:13 +0530

    Updated route to update function

--- designer/router.js
+++ designer/router.js
@@ -4,7 +4,7 @@ module.exports = function(router) {
     router.get('/blueprints',wda.getBlueprints);
     router.delete('/delete/:project_id', wda.deleteBlueprint);
     router.get('/projects', wda.getProjectNames);
-    router.post('/update/:project_id',wda.updateBlueprint);
+    router.put('/blueprints/update/:project_id',wda.updateBlueprint);
     router.get('/user/:project_id',wda.verifyProject);
     router.post('/drafts/create',wda.saveAsDraft);
 }
